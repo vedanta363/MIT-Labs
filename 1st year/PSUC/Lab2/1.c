2646f04ca606d0ec1ad68d9879a2d31d05668523
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-08 16:53:59 +0530

    Create 1.c

---
+++ 1st year/PSUC/Lab2/1.c
@@ -0,0 +1,15 @@
+//Check whether the given number is odd or even
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter any number\n");
+    scanf("%d",&n);
+    if(n%2==0)
+        printf("The number is even");
+    else
+        printf("The number is odd");
+    return 0;
+}
