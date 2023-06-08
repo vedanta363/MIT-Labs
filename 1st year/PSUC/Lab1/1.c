a799efd2320a715be263f5deffa059dc04466dff
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-08 16:52:25 +0530

    Create 1.c

---
+++ 1st year/PSUC/Lab1/1.c
@@ -0,0 +1,13 @@
+//
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    int a,b,sum;
+    printf("Enter two numbers\n");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("the sum of the two numbers is %d ",sum);
+    return 0;
+}
