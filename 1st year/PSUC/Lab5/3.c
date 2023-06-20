4a6af0894370bc011d32b4b2da044886443da7a5
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-21 03:22:08 +0530

    Create 3.c

---
+++ 1st year/PSUC/Lab5/3.c
@@ -0,0 +1,24 @@
+//Insert an element into a 1D array, by getting an element and the position from the user.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n,l,p;
+    printf("Enter the length of array\n");
+    scanf("%d",&l);
+    int a[l+1];
+    printf("Enter the elements of array\n");
+    for(i=0;i<l;i++)
+        scanf("%d",&a[i]);
+    printf("Enter the element and the position to be inserted at\n");
+    scanf("%d%d",&n,&p);
+    for(i=l;i>=p;i--)
+        a[i]=a[i-1];
+    a[p-1]=n;
+    printf("The new array is\n");
+    for(i=0;i<=l;i++)
+        printf("%d\n",a[i]);
+    return 0;
+}
