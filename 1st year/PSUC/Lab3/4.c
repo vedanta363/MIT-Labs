007d1abb9d80ede305d03ea5470633b377a4deef
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-20 16:03:57 +0530

    Create 4.c

---
+++ 1st year/PSUC/Lab3/4.c
@@ -0,0 +1,33 @@
+//Write a program using do-while loop to read the numbers until -1 is encountered. Also count the number of prime numbers 
+//and composite numbers entered by user. [Hint: 1 is neither prime nor composite]
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,f,p=0,c=0;
+    printf("Enter numbers\n");
+    printf("Enter -1 to terminate the loop\n");
+    do
+    {
+        scanf("%d",&n);
+        i=2;
+        f=0;
+        while(i<n)
+        {
+            if(n%i==0)
+            {
+                f=1;
+                break;
+            }
+            i++;
+        }
+        if(f==0&&n!=1&&n!=-1)
+            p++;
+        else if(f==1&&n!=1&&n!=-1)
+            c++;
+    }while(n!=-1);
+    printf("Number of prime nos. are %d\n",p);
+    printf("Number of composite nos. are %d",c);
+    return 0;
+}
