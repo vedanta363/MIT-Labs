6ede8863a0c30f8d1bebfda2a38ee7d74e0e8480
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-20 15:54:28 +0530

    Create 3.c

---
+++ 1st year/PSUC/Lab1/3.c
@@ -0,0 +1,12 @@
+//Write a C program to print the ASCII value of a character
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char x;
+    printf("Enter the character\n");
+    scanf("%c",&x);
+    printf("\nThe ASCII value assigned to the character is %d",x);
+    return 0;
+}
