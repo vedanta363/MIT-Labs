800d1cfa4b3fd017f763b6c93fce6cd47d1c9ae5
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-20 16:03:12 +0530

    Create 3.c

---
+++ 1st year/PSUC/Lab3/3.c
@@ -0,0 +1,22 @@
+//Check if the sum of the cubes of all digits of an inputted number equals the number itself (Armstrong Number). (use while loop)
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,c,arm=0,d;
+    printf("Enter any number \n");
+    scanf("%d",&n);
+    c=n;
+    while(c>0)
+    {
+        d=c%10;
+        arm=arm+d*d*d;
+        c=c/10;
+    }
+    if(arm==n)
+        printf("%d is an armstrong no.",n);
+    else
+        printf("%d is not an armstrong no.",n);
+    return 0;
+}
