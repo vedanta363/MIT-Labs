762ce7b1353b9e98b300a7b21e2f61fe646dc69a
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-20 15:57:42 +0530

    Create 4.c

---
+++ 1st year/PSUC/Lab2/4.c
@@ -0,0 +1,13 @@
+//Find the smallest among three numbers using conditional operator.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,s;
+    printf("Enter any three numbers\n");
+    scanf("%d%d%d",&a,&b,&c);
+    s=(a<b)?((a<c)?a:c):((b<c)?b:c);
+    printf("The smallest of three numbers is %d",s);
+    return 0;
+}
