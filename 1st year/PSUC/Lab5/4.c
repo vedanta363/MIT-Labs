ba6bd773dbff4dea9c82a98effec86e61007129f
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-21 03:22:59 +0530

    Create 4.c

---
+++ 1st year/PSUC/Lab5/4.c
@@ -0,0 +1,29 @@
+//Search the position of the number that is entered by the user and delete that particular number from the array 
+//and display the resultant array elements.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,l,p;
+    printf("Enter the length of array\n");
+    scanf("%d",&l);
+    int a[l];
+    printf("Enter the elements of array\n");
+    for(i=0;i<l;i++)
+        scanf("%d",&a[i]);
+    printf("Enter the position where number is to be deleted at\n");
+    scanf("%d",&p);
+    if(p>l)
+    {
+        printf("Invalid input");
+        exit(0);
+    }
+    for(i=p-1;i<l-1;i++)
+        a[i]=a[i+1];
+    printf("The new array is\n");
+    for(i=0;i<l-1;i++)
+        printf("%d\n",a[i]);
+    return 0;
+}
