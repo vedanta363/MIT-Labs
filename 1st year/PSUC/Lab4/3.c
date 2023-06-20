9b8e0e625bd2a293c1ec6fc394585b6e15f0a5dc
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-21 03:08:06 +0530

    Create 3.c

---
+++ 1st year/PSUC/Lab4/3.c
@@ -0,0 +1,23 @@
+// Evaluate the sine series, sin(x)= x- x^3 /3! + x^5 /5!–x^7 /7!+ ......... to n terms.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int n,i;
+    float x,term,sum,no,pi=3.141592;
+    printf("Enter the value of n and x\n");
+    scanf("%d%f",&n,&x);
+    no=x;
+    x=x*pi/180.0;
+    term=x;
+    sum=x;
+
+    for(i=1;i<n;i++)
+    {
+        term=term*(((-1)*x*x)/(2*i*(2*i+1)));
+        sum+=term;
+    }
+    printf("\nSin(%.2f)=%.2f",no,sum);
+    return 0;
+}
