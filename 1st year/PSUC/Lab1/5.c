683e504cb20404d630e2dd421d5a4fd6d923330d
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-20 15:55:46 +0530

    Create 5.c

---
+++ 1st year/PSUC/Lab1/5.c
@@ -0,0 +1,19 @@
+//Input P, N and R to compute simple and compound interest. [Hint: SI = PNR/100,CI = P(1+R/100)N-P]
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main()
+{
+    int p,r,n;
+    float ci,si,a,b;
+    printf("Enter the values of principle amount, interest and time\n");
+    scanf("%d%d%d",&p,&r,&n);
+    si=p*n*r/100;
+    a=1+(r/100.0);
+    b=pow(a,n);
+    ci=(p*b)-p;
+    printf("\nThe simple interest is %f",si);
+    printf("\nThe compound interest is %f",ci);
+    return 0;
+}
