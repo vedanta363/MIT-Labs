44e5a329c9fcbc3284cf9480b9ac90dd05e520fd
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-06-22 03:20:52 +0530

    Create 1.c

---
+++ 1st year/PSUC/Lab7/1.c
@@ -0,0 +1,22 @@
+//Count the number of words in a sentence.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int w=0,i;
+    char a[1000];
+    printf("Enter a sentence\n");
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(i==0)
+            continue;
+        if(a[i]==' '&&a[i+1]!=' '&&a[i+1]!='\0')
+            w++;
+
+    }
+    printf("The number of words are %d",++w);
+    return 0;
+}
